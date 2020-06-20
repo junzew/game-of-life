06fa84e588829932910a8d028f16a84aa1295cdf
Junze Wu <junzew@hotmail.com>
2020-06-19 19:56:46 -0700

    Work on pyjsdl

--- game.py
+++ game.py
@@ -3,8 +3,74 @@
 #the next line is only needed for python2.x and not necessary for python3.x
 from __future__ import print_function, division
 
-from conway import *
-import pygame
+#import pygame
+import pyjsdl as pygame
+
+
+def num_live_neighbours(i, j, board):
+    """Calculate the number of live neighbour cells"""
+    count = 0
+    m = len(board)
+    n = len(board[0])
+    if (j >= 1):
+        count += board[i][j-1]
+    if (j < n-1):
+        count += board[i][j+1]
+    if (i >= 1):
+        count += board[i-1][j]
+    if (i < m-1):
+        count += board[i+1][j]
+    if (i >= 1 and j >= 1):
+        count += board[i-1][j-1]
+    if (i >= 1 and j < n-1):
+        count += board[i-1][j+1]
+    if (i < m-1 and j >= 1):
+        count += board[i+1][j-1]
+    if (i < m-1 and j < n-1):
+        count += board[i+1][j+1]
+    return count
+
+def make_copy(board):
+    return [row[:] for row in board]
+           
+
+def gameOfLife(board):
+    """
+    Given a board with m by n cells, compute and return the next board state
+    1. Any live cell with fewer than two live neighbors dies
+    2. Any live cell with two or three live neighbors lives
+    3. Any live cell with more than three live neighbors dies
+    4. Any dead cell with exactly three live neighbors becomes a live cell
+    """
+    m = len(board)
+    n = len(board[0])
+    b = make_copy(board)
+    for i in range(m):
+        for j in range(n):
+            nnb = num_live_neighbours(i, j, b)
+            if board[i][j] == 1:
+                if (nnb < 2):
+                    board[i][j] = 0
+                elif (nnb == 2 or nnb == 3):
+                    board[i][j] = 1
+                elif (nnb > 3):
+                    board[i][j] = 0
+            else:
+                if (nnb == 3):
+                    board[i][j] = 1
+
+    return board
+
+
+"""
+start = [
+  [0,1,0],
+  [0,0,1],
+  [1,1,1],
+  [0,0,0]
+]
+print(gameOfLife(start))
+"""
 pygame.init()
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
@@ -15,9 +81,7 @@ background = background.convert()  # prepare for faster blitting
 
 
 clock = pygame.time.Clock()
-mainloop = True
-FPS = 2 # desired framerate in frames per second. try out other values !
-playtime = 0.0
+FPS = 1 # desired framerate in frames per second. try out other values !
 
 r_pentomino = [[0,0,0,0,0,0,0,0],
               [0,0,0,0,0,0,0,0],
@@ -70,21 +134,29 @@ def render_my_game(board):
                 pygame.draw.rect(background, (0,0,0), (SCREEN_WIDTH/2-m * 10+ i*10, SCREEN_HEIGHT/2-n*10 + j*10, 8, 8))
     pygame.display.flip()      # flip the screen like in a flipbook
 
-while mainloop:
-    milliseconds = clock.tick(FPS) # do not go faster than this frame rate
-    playtime += milliseconds / 1000.0
-    # ----- event handler -----
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            mainloop = False # pygame window closed by user
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                mainloop = False # user pressed ESC
 
-    board = gameOfLife(board)
-    render_my_game(board)
-    pygame.display.set_caption("Frame rate: {:0.2f} frames per second."
-                               " Playtime: {:.2} seconds".format(
-                               clock.get_fps(),playtime))
-    #pygame.display.flip()      # flip the screen like in a flipbook
-print("this 'game' was played for %.2f seconds" % playtime)
+def run():
+    mainloop = True
+    playtime = 0.0
+    global board
+    while mainloop:
+        milliseconds = clock.tick(FPS) # do not go faster than this frame rate
+
+        playtime += milliseconds / 1000.0
+        # ----- event handler -----
+        for event in pygame.event.get():
+	    if event.type == pygame.QUIT:
+	        mainloop = False # pygame window closed by user
+	    elif event.type == pygame.KEYDOWN:
+	        if event.key == pygame.K_ESCAPE:
+		    mainloop = False # user pressed ESC
+
+        board = gameOfLife(board)
+        render_my_game(board)
+        #pygame.display.set_caption("Frame rate: {:0.2f} frames per second."
+	#    		           " Playtime: {:.2} seconds".format(
+	#    		           clock.get_fps(),playtime))
+        #pygame.display.flip()      # flip the screen like in a flipbook
+
+#run()
+pygame.display.setup(run)
